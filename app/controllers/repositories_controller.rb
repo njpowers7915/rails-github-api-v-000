5bdfa8dcdc81324d315ae33884d773518da9f315
Nicholas Powers <njpowers7915@gmail.com>
2018-08-13 12:36:07 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -14,8 +14,8 @@ class RepositoriesController < ApplicationController
   def create
     resp = Faraday.post("https://api.github.com/uer/repos") do |req|
       req.params['oauth_token'] = session[:token]
-      req.params['v'] = '3'
-      req.params['name'] = params[:venue_id]
+      #req.params['v'] = '3'
+      req.params['name'] = params[:name]
     end
 
     redirect_to repositories_path
