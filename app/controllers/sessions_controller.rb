54d49d3eb8854edb7e67c1fe8d3058c14d5ec7a2
Nicholas Powers <njpowers7915@gmail.com>
2018-08-13 11:36:41 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,18 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    session[:token] = params["access_token"]
-    resp = Faraday.get("https://github.com/user?access_token='<%= params["access_token"] %>'") do |req|
+    #session[:token] = params["access_token"]
+    resp = Faraday.get("https://github.com/user?access_token=#{params["access_token"]}") do |req|
       req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
       req.params['client_secret'] = ENV['GITHUB_SECRET']
+      req.params['code'] =
       #req.params['grant_type'] = 'authorization_code'
       req.params['redirect_uri'] = "http://localhost:3000/auth"
       req.params['code'] = params[:code]
     end
 
     body = JSON.parse(resp.body)
-    #session[:token] = body["access_token"]
+    session[:token] = body[:access_token]
     redirect_to root_path
   end
 end
